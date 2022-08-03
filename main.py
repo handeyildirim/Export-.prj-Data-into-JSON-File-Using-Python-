924456bc2ba49ee725b4400a8eab3265d033c415
Hande Yıldırım <handeyildirim1996@gmail.com>
2022-08-03 14:57:15 +0200

    create functions according to their special roles

--- main.py
+++ main.py
@@ -3,22 +3,37 @@
 import os.path
 from pathlib import Path
 
-in_file = "conti_scal.prj"
+file_data = {}
 
-out_dir = os.path.join(os.getcwd(), ".\json\\")
-out_file = "testcases_scc_full.json"
-out_file_path = out_dir + out_file
 
-if os.path.exists(in_file):
-    conti_scal = open(out_file_path, 'r')
-    print(conti_scal)
+def check_input_file():
+    in_file = "conti_scal.prj"
+    if os.path.exists(in_file):
+        conti_scal = open(in_file, 'r')
+        print(conti_scal)
 
-if not os.path.exists(out_dir):
-    print("Creating directory...")
-    Path(out_dir).mkdir(parents=True, exist_ok=True)
+
+def create_a_json_file_for_outputs(out_file_path):
     print("Creating json file...")
     testcases_scc_full = open(out_file_path, 'w')
-else:
-    if not os.path.exists(out_file_path):
-        print("Creating json file...")
-        testcases_scc_full = open(out_file_path, 'w')
+
+
+def create_a_folder_for_output_files(out_dir):
+    print("Creating directory...")
+    Path(out_dir).mkdir(parents=True, exist_ok=True)
+
+
+def check_output_file():
+    check_input_file()
+    out_dir = os.path.join(os.getcwd(), ".\json\\")
+    out_file = "testcases_scc_full.json"
+    out_file_path = out_dir + out_file
+    if not os.path.exists(out_dir):
+        create_a_folder_for_output_files(out_dir)
+        create_a_json_file_for_outputs(out_file_path)
+    else:
+        if not os.path.exists(out_file_path):
+            create_a_json_file_for_outputs(out_file_path)
+
+
+check_output_file()
